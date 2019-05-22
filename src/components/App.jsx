c85cf0776c331a58b8e96eac40b4045f8d2bce42
shohel <cse_shohel@yahoo.com>
2019-05-22 11:35:57 +0600

    define the initial folder and file structure

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "../styles/logo.svg";
+import "./App.scss";
+
+import Navbar from '../components/Navbar/Navbar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            Welcome to React app
+          </h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  
+  render() {
+    return (
+        <div></div>
+    );
+  }
+}
+
+export default Navbar;
